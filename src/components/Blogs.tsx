b85b0b697bf40ddacd05d188492c427b687f3b8c
b2230356007@cs.hacettepe.edu.tr <osmerdmdtr@gmail.com>
2026-05-17 14:56:05 +0300

    Added two blogs and made fixes

--- src/components/Blogs.tsx
+++ src/components/Blogs.tsx
@@ -13,13 +13,13 @@ interface Blog {
 
 const blogs: Blog[] = [
   {
-    slug: 'ctf-solution-exploiting-wordpress-royal-elementor-to-root-gridloy',
+    slug: 'CTF-Solution-wordpress-royal-elementor-to-root-gridloy',
     title: 'CTF Çözümü: WordPress Royal Elementor Exploit ile Root’a Erişim',
-    excerpt: 'Gridloy makinesi üzerinde WordPress servis keşfi, Royal Elementor eklentisindeki zafiyetin istismarı, ilk erişim elde edilmesi ve root yetkisine kadar ilerleyen privilege escalation adımlarının incelendiği bu write-up.',
+    excerpt: 'Örnek bir senaryo üzerinde WordPress servis keşfi, Royal Elementor eklentisindeki zafiyetin istismarı, ilk erişim elde edilmesi ve root yetkisine kadar ilerleyen privilege escalation adımlarının incelendiği bu write-up.',
     author: 'Osman Erdem Dutar',
   },
   {
-    slug: 'ctf-solution-information-disclosure-and-privilege-escalation-on-the-machine',
+    slug: 'CTF-Solution-Information-Disclosure-and-Privilege-Escalation-on-the-Machine',
     title: 'CTF Çözümü: Information Disclosure ve Privilege Escalation',
     excerpt: 'Senaryo üzerinde bilgi ifşası, açıkta bırakılan geliştirme dizinleri, parola tekrar kullanımı ve hatalı sudo yetkilendirmesi üzerinden ilerlenen bu write-up\'ta; keşif, ilk erişim, lateral movement ve privilege escalation adımları incelenmektedir.',
     author: 'Osman Erdem Dutar',
